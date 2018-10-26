28b43133d93a64c03de20984c6b035d944f73d88
curx95 <curx95@gmail.com>
2018-10-26 14:23:21 +0800

    del autoplay

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -16,8 +16,7 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true,
-        autoplay: 5000
+        loop: true
       },
       swiperList: [
         {
